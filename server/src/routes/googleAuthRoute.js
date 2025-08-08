801ab4d2933c58addb9d5d37efd97be17fc7a097
afid0n <fidanza@code.edu.az>
2025-08-08 19:20:36 +0400

    feat:google backend login-register

---
+++ server/src/routes/googleAuthRoute.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const passport = require("passport");
+const { CLIENT_URL } = require("../config/config");
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${CLIENT_URL}/auth/login?error=google_failed`,
+    session: false,
+  }),
+  (req, res) => {
+    const jwt = require("../utils/jwt");
+    const accessToken = jwt.generateAccessToken({
+      id: req.user._id,
+      email: req.user.email,
+      fullName: req.user.fullName,
+    });
+ 
+
+
+    res.redirect(`${CLIENT_URL}/auth/success/#token=${accessToken}`);
+  }
+);
+
+module.exports = router;
